e7b46396d0c815c7e72a8fd295d8ea130e421ba5
sbx.7 <sbx7@purelymail.com>
2024-12-06 12:00:46 -0800

    Multiple fetures added

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from tkinter import Tk, Canvas
 from datetime import date, datetime
+from playsound import playsound
 
+# Window init
 root = Tk()
 c = Canvas(root, width=800, height=800, bg='black')
 root.resizable(False, False)
-root.title("Christmas Countdown 🎄")
+root.title("Christmas Countdown")
+root.iconbitmap("window_icon.ico")
 c.pack()
 
-c.create_text(100, 50, anchor='w', fill='orange', font='Arial 28 bold underline', text='test')
+#Audio
+playsound('audio/bells.wav')
+
+#Text creation
+c.create_text(100, 50, anchor='w', fill='orange', font='Arial 28 bold', text='Christmas countdown🎄')
 
 root.mainloop()
