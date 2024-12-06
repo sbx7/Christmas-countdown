4dad91938a5c5a5f24fdeaa0352d9277a36eba38
sbx.7 <sbx7@purelymail.com>
2024-12-06 14:19:18 -0800

    Debug: made sound play in a new thread

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from tkinter import Tk, Canvas
 from datetime import date, datetime
-from playsound import playsound
+from playsound3 import playsound
+import threading
+
+def play_sound():
+    # Play sound in a separate thread
+    playsound("audio/bells.wav")
 
 # Window init
 root = Tk()
-c = Canvas(root, width=800, height=800, bg='black')
+c = Canvas(root, width=600, height=400, bg='red')
 root.resizable(False, False)
 root.title("Christmas Countdown")
 root.iconbitmap("window_icon.ico")
 c.pack()
 
-#Audio
-playsound('audio/bells.wav')
+# Start the sound playback in a new thread
+sound_thread = threading.Thread(target=play_sound)
+sound_thread.start()
 
 #Text creation
-c.create_text(100, 50, anchor='w', fill='orange', font='Arial 28 bold', text='Christmas countdown🎄')
+c.create_text(100, 50, anchor='w', fill='white', font='Arial 28 bold', text='Christmas countdown🎄')
 
 root.mainloop()
