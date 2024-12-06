d2542b9b8adf816ef24b3b8f16ea38c1d2ceb09e
sbx.7 <sbx7@purelymail.com>
2024-12-06 09:58:35 -0800

    Added window title

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from datetime import date, datetime
 root = Tk()
 c = Canvas(root, width=800, height=800, bg='black')
 root.resizable(False, False)
+root.title("Christmas Countdown 🎄")
 c.pack()
 
 c.create_text(100, 50, anchor='w', fill='orange', font='Arial 28 bold underline', text='test')
