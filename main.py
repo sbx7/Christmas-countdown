38767092515818a8a717f4df1ecf5dad74c07d83
supersnowbox <sbx7@purelymail.com>
2025-01-14 17:21:34 -0800

    Finnished get_events, still does nothing

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
+print("Loading...")
 from tkinter import Tk, Canvas
 from datetime import date, datetime
 from playsound3 import playsound
 import threading
 
+
 def play_sound():
     # Play sound in a separate thread
     playsound("audio/bells.wav")
 
+
+def get_events():
+    list_events=[]
+    with open(events) as file:
+        for line in file:
+            line=line.rstrip('\n')
+            current_event=line.split('.')
+            
+
 # Window init
 root = Tk()
-c = Canvas(root, width=600, height=400, bg='red')
+c = Canvas(root, width=800, height=800, bg='red') # Will change reslution later 
 root.resizable(False, False)
 root.title("Christmas Countdown")
 root.iconbitmap("window_icon.ico")
@@ -22,4 +33,5 @@ sound_thread.start()
 #Text creation
 c.create_text(100, 50, anchor='w', fill='white', font='Arial 28 bold', text='Christmas countdown🎄')
 
+print("Done loading")
 root.mainloop()
